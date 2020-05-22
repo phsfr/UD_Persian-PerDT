5e0d43d83a340e18ba87ef124525a1bf418a02c0
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-05-22 18:03:23 -0400

    Reversing VCONJ in the Dadegan treebank

--- fix_dadegan_deps.py
+++ fix_dadegan_deps.py
@@ -1,9 +1,62 @@
 """
 * Fix "wa"s that are tagged as SUBR
 * Change wrong dependencies in PARCL.
+* Change VConj order.
 """
 
 from dep_tree import DependencyTree
+
+
+def fix_verb_conj_order(tree, v, label):
+    v_head = tree.heads[v] - 1
+    v_grand_head = tree.heads[v_head]
+    tree.heads[v] = v_grand_head
+    tree.labels[v] = tree.labels[v_head]
+    tree.heads[v_head] = v + 1
+    tree.labels[v_head] = label
+    tree.rebuild_children()
+
+
+def fix_cc_order(tree, cc, label):
+    if len(tree.children[cc + 1]) == 0:
+        print("Warning: No CC children", tree.other_features[0].feat_dict['senID'])
+        return
+
+    children = sorted(list(tree.children[cc + 1]))
+    cc_dep = None
+    for c in children:
+        child = c - 1
+        if child < cc + 1 and tree.tags[child] in {"ADR", "V", "PSUS", "N", "PREM", "ADJ"}:
+            cc_dep = child
+
+    if cc_dep is None:
+        print("Orphaned", tree.other_features[0].feat_dict['senID'])
+        return
+
+
+    cc_head = tree.heads[cc] - 1
+    cc_grand_head = tree.heads[cc_head]
+    cc_grand_label = tree.labels[cc_grand_head - 1] if cc_grand_head > 0 else "ROOT"
+    tree.heads[cc_dep] = cc_grand_head
+    tree.labels[cc_dep] = cc_grand_label
+    tree.heads[cc_head] = cc + 1
+    tree.labels[cc_head] = "POSDEP"
+    tree.heads[cc] = cc_dep + 1
+    tree.labels[cc] = label
+    tree.rebuild_children()
+
+
+def fix_vconj_order(tree):
+    vconj_list = []
+    for i in range(len(tree.labels) - 1, -1, -1):
+        label = tree.labels[i]
+        if label == "VCONJ":
+            if tree.tags[i] == "CONJ":
+                fix_cc_order(tree, i, tree.labels[i])
+            else:
+                fix_verb_conj_order(tree, i, tree.labels[i])
+            vconj_list.append((i, tree.heads[i], tree.tags[i], tree.words[i]))
+
 
 if __name__ == '__main__':
     input_files = ['Persian_Dependency_Treebank_(PerDT)_V1.1.1/Data/train.conll',
@@ -15,16 +68,19 @@ if __name__ == '__main__':
 
     for f_idx, inp_f in enumerate(input_files):
         parcl_trees = []
+        vconj_trees = []
         tree_list = DependencyTree.load_trees_from_conll_file(inp_f)
         for i, tree in enumerate(tree_list):
-            for w, word in enumerate(tree.words):
-                if word == "و" and tree.tags[w] == "SUBR":
-                    tree.tags[w] = "CONJ"
-                    tree.ftags[w] = "CONJ"
-            if "PARCL" in tree.labels:
-                parcl_trees.append(tree)
+            # for w, word in enumerate(tree.words):
+            #     if word == "و" and tree.tags[w] == "SUBR":
+            #         tree.tags[w] = "CONJ"
+            #         tree.ftags[w] = "CONJ"
+            # if "PARCL" in tree.labels:
+            #     parcl_trees.append(tree)
+            if "VCONJ" in tree.labels:
+                vconj_trees.append(tree)
+                fix_vconj_order(tree)
 
-        print("Visiting parcl trees")
         for tree in parcl_trees:
             include_tree = False
             parcl_idx = [i for i, label in enumerate(tree.labels) if label == "PARCL"]
@@ -37,6 +93,6 @@ if __name__ == '__main__':
                         if tree.labels[dep] not in {"PREDEP", "PARCL", "MOS", "NVE", "VPP", "PUNC", "OBJ", "NPP",
                                                     "VCONJ"}:
                             if tree.labels[dep] in {"SBJ", "AJUCL", "ADV"}:
-                                # Change the head for SBJ/AJUCL
+                                # Change the head for SBJ/AJUCL/ADV
                                 tree.heads[dep] = idx + 1
         DependencyTree.write_to_conll(tree_list, output_files[f_idx])
