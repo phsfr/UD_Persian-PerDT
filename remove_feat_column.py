95694da7e08dd0bf507ef50edd2579ee814065ec
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-05-27 15:58:26 -0400

    Script for removing feats

---
+++ remove_feat_column.py
@@ -0,0 +1,24 @@
+import sys
+
+from dep_tree import *
+
+# For removing feature column!
+
+if __name__ == '__main__':
+    input_folder = os.path.abspath(sys.argv[1])
+    output_folder = os.path.abspath(sys.argv[2])
+
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+
+    for file in os.listdir(input_folder):
+        input_path = os.path.join(input_folder, file)
+        output_path = os.path.join(output_folder, file)
+        print(input_path)
+        univ_trees: List[DependencyTree] = DependencyTree.load_trees_from_conllu_file(input_path)
+        for t, univ_tree in enumerate(univ_trees):
+            for l, label in enumerate(univ_tree.labels):
+                univ_tree.other_features[l].feat_str = "_"
+                univ_tree.ftags[l] = "_"
+
+        DependencyTree.write_to_conllu(univ_trees, output_path)
