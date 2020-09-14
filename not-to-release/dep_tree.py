e5d4a2b89f748568c802a76ce2c45554a8a5da33
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-14 18:07:31 -0400

    Fixed "fixed" for case problem

--- not-to-release/dep_tree.py
+++ not-to-release/dep_tree.py
@@ -1512,7 +1512,12 @@ class DependencyTree:
                 self.labels[i] = "obl"
             if (self.tags[i] == "PUNCT" and self.words[i]!="-") and self.heads[i]>i:
                 self.final_tags[i] = "SpaceAfter=No"
-            if self.heads[i]>0 and self.labels[self.heads[i]-1] in {"aux", "aux:pass", "cop", "case"}:
+            if self.heads[i]>0 and self.labels[self.heads[i]-1] in {"aux", "aux:pass", "cop"}:
+                self.heads[i] = self.heads[self.heads[i]-1]
+            if self.heads[i]>0 and self.labels[self.heads[i]-1] in {"case"} and self.labels[i]=="fixed":
+                self.heads[i] = self.heads[self.heads[i]-1]
+                self.labels[i] = "case"
+            if self.heads[i]>0 and self.labels[self.heads[i]-1] in {"case"} and self.labels[i]!="fixed":
                 self.heads[i] = self.heads[self.heads[i]-1]
         if self.sen_id == 23558:
             self.heads[16] = 19
